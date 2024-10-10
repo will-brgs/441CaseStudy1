39f67873887cf7a498be3bfa522ce84b512472e0
Will Burgess <112338817+will-brgs@users.noreply.github.com>
2024-10-10 18:45:29 -0500

    Add files via upload

---
+++ CS1.m
@@ -0,0 +1,7 @@
+%% ESE441 Case Study 1: 
+%% Introduction
+% * Authors:                  Will Burgess, Mack LaRosa
+% * Class:                    ESE 441
+% * Date:                     Created 10/10/2024, Last Edited 10/18/2024
+%%
+code = "finished";
