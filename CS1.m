e804a13ee0d910b88c545186234bb170a9202c80
Will <Will@Will-laptop.wusm-wifi.wucon.wustl.edu>
2024-10-11 14:32:31 -0500

    first of many

--- CS1.m
+++ CS1.m
@@ -5,3 +5,13 @@
 % * Date:                     Created 10/10/2024, Last Edited 10/18/2024
 %%
 code = "finished";
+v = 2; % V1, infection rate
+k = [1,2].';% Sat constant for: infection, recovery
+r = 0.1; % recovery rate
+
+x = [1e6,1]; % Initial: susceptible, infected individuals
+a = 1e-3; % Rate of reinfection/loss of immunity
+u = [0,0]; % Control inputs
+
+xdot = [-1*((v * x(1) * x(2))/(k(1)+x(2)))-a*x(2) + u(1);
+    ((v * x(1) * x(2))/(k(1) + x(2))) - (r * x(2))/(x(2) + k(2)) - a*x(2) + u(2)];
