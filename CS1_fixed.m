aa81c9fbbef67be776aba4491de015268ba2ffa8
Will <Will@Will-laptop.dhcp.wustl.edu>
2024-10-18 13:50:45 -0500

    file saving fix

--- CS1_fixed.m
+++ CS1_fixed.m
@@ -9,7 +9,7 @@ clear
 clc
 code = "finished";
 %% Part 1: Model Sim using ODE45
-figure;
+fh1 = figure(1);
 for i =1:4
     if i == 1
     v = 0.1; % V1, infection rate (between 0 and 1)
@@ -42,8 +42,6 @@ system = @(t, x) [-1*((v * x(1) * x(2))/(k(1)+x(2)))+ a * x(2) + u(1);
 
 [t, x] = ode45(system, t, IC);
 
-%fh1 = figure(1)
-
 subplot(2,2,i)
 plot(t, x(:,1), 'linewidth', 1.5);
 hold on;
@@ -61,7 +59,7 @@ end
 % zoom in on oscilations at endemic state
 x = x(75:151,:);
 t = t(75:151);
-figure
+fh2 = figure(2);
 subplot(2,1,1)
 plot(t, x(:,1), 'linewidth', 1.5);
 xlabel('Time (days)');
@@ -80,7 +78,6 @@ title('Infected Individuals')
 sgtitle({'Zero-input Simulation #4 at Endemic State'},'FontSize', 12, 'FontWeight', 'bold')
 grid on
 %% Part 1: Simulate System with Linearized Model at Xeq
-figure;
 for i =1:4
     if i == 1
     v = 0.1; % V1, infection rate (between 0 and 1)
@@ -117,7 +114,7 @@ system = @(t, x) J * x;
 
 [t, x] = ode45(system, t, IC);
 
-%fh2 = figure(1)
+fh3 = figure(3);
 subplot(2,2,i)
 plot(t, x(:,1), 'linewidth', 1.5);
 hold on;
@@ -132,7 +129,6 @@ legend('Susceptible','Infected');
 grid on
 end
 %% Part 1: Simulate System with Linearized Model Above Xeq
-figure;
 for i =1:4
     if i == 1
     v = 0.1; % V1, infection rate (between 0 and 1)
@@ -169,7 +165,7 @@ system = @(t, x) J * x;
 
 [t, x] = ode45(system, t, IC);
 
-%fh2 = figure(1)
+fh4 = figure(4);
 subplot(2,2,i)
 plot(t, x(:,1), 'linewidth', 1.5);
 hold on;
@@ -209,7 +205,7 @@ wave(ivStartTime:(ivStartTime + length(sine)-1),1) = sine;
 u = cat(2,wave(:), zeros(length(t),1));
 
 % Plot control input
-figure
+fh5 = figure(5);
 plot(t,u(:,1), 'linewidth', 2.5)
 hold on
 plot(t,u(:,2), 'linewidth', 1.5)
@@ -225,8 +221,7 @@ system = @(t, x) [-1*((v * x(1) * x(2))/(k(1)+x(2)))+ a * x(2) + u(round(t+1),1)
 
 [t, x1] = ode45(system, t, IC);
 
-% fh1 = figure(1);
-figure
+fh6 = figure(6);
 plot(t, x1(:,1), 'linewidth', 1.5);
 hold on;
 plot(t, x1(:,2), 'linewidth', 1.5);
@@ -248,36 +243,23 @@ beta = 0.15;
 IC = [1e6 - 10,10]; %Initial susceptible, Initial infected 
 t = 0:1:150;
 
-ivMax = 10000;
-ivPeriod = 70; %days the intervention will last
-ivStartTime = 20; % day you want to start control input
-
-% Initialize control input (wave)
-wave = zeros(length(t),1);
-
-% Generate sine wave with given amplitude and period
-sine = ivMax * sin(pi * (1/ivPeriod) * (0:(ivPeriod))); %pi instead of 2pi to get 1 half period
-
-% Insert sine wave at specified start time
-wave(ivStartTime:(ivStartTime + length(sine)-1),1) = sine;
-
 %These control inputs can control at what population an endemic state is
 %reached by changing beta
-system_endemic = @(t, x) [-1*((v * x(1) * x(2))/(k(1)+x(2)))+ a*x(2) + beta*x(2); 
+sysEndemic = @(t, x) [-1*((v * x(1) * x(2))/(k(1)+x(2)))+ a*x(2) + beta*x(2); 
     ((v * x(1) * x(2))/(k(1) + x(2))) - (r * x(2))/(x(2) + k(2)) - a*x(2) - beta*x(2)];
 
 %Modeling cannibal zombies will drive disease to eradication
-system_zombies = @(t, x) [-1*((v * x(1) * x(2))/(k(1)+x(2)))+ a*x(2); 
+sysZombies = @(t, x) [-1*((v * x(1) * x(2))/(k(1)+x(2)))+ a*x(2); 
     ((v * x(1) * x(2))/(k(1) + x(2))) - (r * x(2))/(x(2) + k(2)) - a*x(2) - beta*x(2)];
 
-[t_zombies, x1_zombies] = ode45(system_zombies, t, IC);
-[t_endemic, x1_endemic] = ode45(system_endemic, t, IC);
+[tZombies, xZombies] = ode45(sysZombies, t, IC);
+[tEndemic, xEndemic] = ode45(sysEndemic, t, IC);
 
-figure
+fh7 = figure(7);
 subplot(2, 1, 1)
 hold on;
-plot(t_zombies, x1_zombies(:,1), 'linewidth', 1.5);
-plot(t_zombies, x1_zombies(:,2), 'linewidth', 1.5);
+plot(tZombies, xZombies(:,1), 'linewidth', 1.5);
+plot(tZombies, xZombies(:,2), 'linewidth', 1.5);
 xlabel('Time (days)');
 ylabel('# of Individuals');
 legend('Susceptible','Infected');
@@ -287,8 +269,8 @@ grid on
 
 subplot(2, 1, 2)
 hold on;
-plot(t_endemic, x1_endemic(:,1), 'linewidth', 1.5);
-plot(t_endemic, x1_endemic(:,2), 'linewidth', 1.5);
+plot(tEndemic, xEndemic(:,1), 'linewidth', 1.5);
+plot(tEndemic, xEndemic(:,2), 'linewidth', 1.5);
 xlabel('Time (days)');
 ylabel('# of Individuals');
 legend('Susceptible','Infected');
@@ -336,8 +318,7 @@ system = @(t, x) [-1*((v * x(1) * x(2))/(k(1)+x(2)))+ a * x(2) + u(1) - C(1,2)*(
 
 [t, x] = ode45(system, t, IC);
 
-figure()
-figure;
+fh8 = figure(8);
 for i = 1:N
     subplot(N, 1, i);
     plot(t, x(:, 2*i-1), 'linewidth', 1.5);
@@ -404,7 +385,7 @@ system = @(t, x) [-1*((v * x(1) * x(2))/(k(1)+x(2)))+ a * x(2) + u(round(t+1),1)
 
 [t, x] = ode45(system, t, IC);
 
-figure
+fh9 = figure(9);
 for i = 1:N
     subplot(N, 1, i);
     plot(t, x(:, 2*i-1), 'linewidth', 1.5);
@@ -423,80 +404,14 @@ end
 sgtitle({'Networked Model Simulation #2', ...
     sprintf('V_{1} =%.1f, K_{1} =%.1d, K_{2} =%.1d, r =%.1f, \\alpha =%.4f', v, k(1), k(2), r, a)},...
     'FontSize', 12, 'FontWeight', 'bold')
-%% Auxilliary Code : Meant Saving Information and Storing Old Code
-% -------------------------------------------------------------------
 %% Save images
 % filepath = "C:\Users\Will\OneDrive - Washington University in St. Louis\. Control Systems\Case Study 1\Figure export";
 % exportgraphics(fh1, fullfile(filepath, 'part1 different vars.jpg'), 'resolution', 300);
 % exportgraphics(fh2, fullfile(filepath, 'part1 equilibrium zoom in.jpg'), 'resolution', 300);
-% exportgraphics(fh3, fullfile(filepath, 'part1 linearized sim.jpg'), 'resolution', 300);
-% exportgraphics(fh4, fullfile(filepath, 'part2 delayed sine input u.jpg'), 'resolution', 300);
-% exportgraphics(fh5, fullfile(filepath, 'part2 delayed sine sim.jpg'), 'resolution', 300);
-% exportgraphics(fh6, fullfile(filepath, 'part2 zombies sim.jpg'), 'resolution', 300);
-% exportgraphics(fh7, fullfile(filepath, 'part3 networked sim set 1'), 'resolution', 300);
-% exportgraphics(fh8, fullfile(filepath, 'part3 networked sim set 2'), 'resolution', 300);
-% exportgraphics(fh0, fullfile(filepath, 'part3 networked sim set 3'), 'resolution', 300);
-
-%% Archive: Old Code That Might be Useful
-% -------------------------------------------------------------------
-% v = 0.05; % V1, infection rate (between 0 and 1)
-% k = [100000,1].';% Sat constant for: infection, recovery IMPORTANT CONSTRAINT
-% r = 1000; % recovery rate
-% 
-% x = [1e6,1]; % Initial: susceptible, infected individuals
-% a = -0.01; % Rate of reinfection/loss of immunity (hundreds place)
-% u = [0,3e3]; % Control inputs
-% 
-% xdot = [-1*((v * x(1) * x(2))/(k(1)+x(2)))+ a * x(2) + u(1);
-%     ((v * x(1) * x(2))/(k(1) + x(2))) - (r * x(2))/(x(2) + k(2)) - a*x(2) + u(2)];
-
-% xpplane = [-1*((v * x * y)/(k+y))+ a * y;
-%     ((v * x * y)/(k + y) - (r * y)/(y + l) - a*y];
-%y = x2. k = k1, l = k2
-
-% fh2 = figure(2);
-% plot(x1(:,1), x1(:,2), 'linewidth', 1.5);
-% title({'Zero-Input Simulation #1 (Trace)',sprintf('V_{1} =%.1f, K_{1} =%.1f, K_{2} =%.1f, r =%.1f, \\alpha =%.4f', v, k(1), k(2), r, a)});
-% xlabel('X_{1}');
-% ylabel('X_{2}');
-% grid on
-
-% -------- NETWORKED MODEL FUNC
-% function xDot = networkedModel(~, x, N, v, k, r, a, u, C, D)
-% 
-% xDot = zeros(2 * N, 1);
-%     
-%     for i = 1:N
-%         x1iIndex = 2 * i - 1;
-%         x2iIndex = 2 * i;
-%         
-%         x1Sum = 0;
-%         x2Sum = 0;
-% 
-%         for j = 1:N
-%             if j ~= i
-%                 x1jIndex = 2 * j - 1;
-%                 x2jIndex = 2 * j;
-%                 
-%                 % Summing over differences between regions
-%                 x1Sum = x1Sum + C(i,j) * (x(x1iIndex) - x(x1jIndex));
-%                 x2Sum = x2Sum + D(i,j) * (x(x2iIndex) - x(x2jIndex));
-%             end
-%         end
-%         
-%         % Susceptible population equation (x1i)
-%         x1Dot = -1 * (v * x(x1iIndex) * x(x2iIndex))/(k(1) + x(x2iIndex)) ...
-%                 + a * x(x2iIndex) + u(i, 1) - x1Sum;
-%         
-%         % Infected population equation (x2i)
-%         x2Dot = (v * x(x1iIndex) * x(x2iIndex))/(k(1) + x(x2iIndex)) ...
-%                 - (r * x(x2iIndex)) / (x(x2iIndex) + k(2)) ...
-%                 - a * x(x2iIndex) + u(i, 2) - x2Sum;
-%         
-%         % Store for output
-%         xDot(x1iIndex) = x1Dot;
-%         xDot(x2iIndex) = x2Dot;
-%     end
-% end
-%---------------- 
-%[t, x] = ode45(@(t, x) networkedModel(t, x, N, v, k, r, a, u, C, D), tspan, IC1);
+% exportgraphics(fh3, fullfile(filepath, 'part1 linearized sim 1.jpg'), 'resolution', 300);
+% exportgraphics(fh4, fullfile(filepath, 'part1 linearized sim 2.jpg'), 'resolution', 300);
+% exportgraphics(fh5, fullfile(filepath, 'part2 delayed sine input u.jpg'), 'resolution', 300);
+% exportgraphics(fh6, fullfile(filepath, 'part2 delayed sine sim.jpg'), 'resolution', 300);
+% exportgraphics(fh7, fullfile(filepath, 'part2 zombies sim.jpg'), 'resolution', 300);
+% exportgraphics(fh8, fullfile(filepath, 'part3 networked sim no control.jpg'), 'resolution', 300);
+% exportgraphics(fh9, fullfile(filepath, 'part3 networked sim control.jpg'), 'resolution', 300);
